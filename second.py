749b090189c94f440937338d8ee89bbc40c7cd61
OmriKepes <66702096+OmriKepes@users.noreply.github.com>
2020-06-10 10:31:23 +1200

    Create second.py

---
+++ second.py
@@ -0,0 +1,7 @@
+'''This program does variations and inputs.'''
+
+#variations can store one piece of infomation
+#name them well, lower case, underscore between words
+
+name = input("Whats your name?")
+print("Hello {} how are you?".format(name))
