8df69a7c2e154c0ebd7228d3749d166d0e01434c
OmriKepes <66702096+OmriKepes@users.noreply.github.com>
2020-06-11 14:04:38 +1200

    task3

--- task 1.py
+++ task 1.py
@@ -3,7 +3,19 @@
 #variations can store one piece of infomation
 #name them well, lower case, underscore between words
 
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+ask= True
+counter=0
+old_num=0
+while ask== True:
+    num = int(input("Enter number: "))
+    if num == 0:
+        ask = False
+    else:
+        counter+=1
+        old_num+=num
+avg=old_num/counter
+avgr=round(avg,1)
+print (avgr)
+        
 
-print(num1*num2)
+
