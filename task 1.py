9727d7296f44b4bd462cb883d063957e98a8c638
OmriKepes <66702096+OmriKepes@users.noreply.github.com>
2020-06-11 14:20:39 +1200

    task3

--- task 1.py
+++ task 1.py
@@ -3,17 +3,9 @@
 #variations can store one piece of infomation
 #name them well, lower case, underscore between words
 
-ask= True
-num1 = int(input("Enter first number: "))
-counter = 0
 total_add = 0
-while ask==True:
+for i in range(int(input("Enter first number: "))):
     num=int(input("Number: "))
-    counter = counter+1
-    if counter<num1:
-        total_add = total_add+num
-    else:
-        total_add = total_add+num
-        ask= False
+    total_add = total_add+num
 print (total_add)
     
