bb74942e75d1958d5ebddb20c6f42dacdce71936
OmriKepes <66702096+OmriKepes@users.noreply.github.com>
2020-06-11 13:46:42 +1200

    task1

--- task 1.py
+++ task 1.py
@@ -3,6 +3,5 @@
 #variations can store one piece of infomation
 #name them well, lower case, underscore between words
 
-num = int(input("Enter number: ")
+num = int(input("Enter number: "))
 print(num*2)
-          
