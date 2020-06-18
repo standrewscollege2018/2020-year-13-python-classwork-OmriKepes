efe8ce7f1b782a2a6475094afcf393143584284c
OmriKepes <66702096+OmriKepes@users.noreply.github.com>
2020-06-18 14:15:45 +1200

    Update studentmanagment.py

--- studentmanagment.py
+++ studentmanagment.py
@@ -1,8 +1,8 @@
 ''' This program stores students names and classes.'''
 
-class stduent:
+class students:
     '''This class (student) has a name, class, age, phone number, gender.'''
-    def __init__(self, name, age, phoneNumber, gender):
+    def __init__(self, name, age, phone_number, gender):
         '''This funcation runs all at the start and sets up all atributes.'''
         self._name = name
         self._age = age
@@ -20,10 +20,13 @@ class stduent:
         return(self._gender)
     def get_phoneNumber(self):
         '''This funcation returns the phoneNumber of the student.'''
-        return(self._phoneNumber)
+        return(self._phone_number)
 def create_student():
     '''This funcation allows the user to input a new students name, age, gender, phone number.'''
     new_name = input("Enter the new Students name: ")
     new_age = int(input("Enter the new students age: "))
-    new_gender = input("Enter gender (F or F): ")
-    new_phoneNumber = int(input("Enter phone number: "))
+    new_gender = bool(input("Enter gender (M or F): "))
+    new_phone_number = int(input("Enter phone number: "))
+
+#the list of students enrolled
+student_list=[]
